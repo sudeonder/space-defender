1cf001475f10bee2272dcde2ecc003ba36709f0f
Sude Önder <sudeonder999@gmail.com>
2024-04-02 20:02:20 +0300

    add screen boundaries for player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,16 +9,40 @@ public class Player : MonoBehaviour
     private Vector2 rawInput;
     [SerializeField]
     private float moveSpeed = 5f;
+    private Vector2 minBounds;
+    private Vector2 maxBounds;
+    [SerializeField]
+    private float leftPadding;
+    [SerializeField]
+    private float rightPadding;
+    [SerializeField]
+    private float topPadding;
+    [SerializeField]
+    private float bottomPadding;
+
+    void Start()
+    {
+        InitBounds();
+    }
     
     void Update()
     {
         Move();  
     }
 
+    private void InitBounds()
+    {
+        minBounds = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 0));
+        maxBounds = Camera.main.ViewportToWorldPoint(new Vector3(1, 1, 0));
+    }
+
     void Move()
     {
-        Vector3 delta = rawInput;
-        transform.position += delta * moveSpeed * Time.deltaTime;
+        Vector2 delta = rawInput * moveSpeed * Time.deltaTime;
+        Vector2 newPos = new Vector2();
+        newPos.x = Mathf.Clamp(transform.position.x + delta.x, minBounds.x + leftPadding, maxBounds.x - rightPadding);
+        newPos.y = Mathf.Clamp(transform.position.y + delta.y, minBounds.y + topPadding, maxBounds.y - bottomPadding);
+        transform.position = newPos;
     }
 
     void OnMove(InputValue value)
